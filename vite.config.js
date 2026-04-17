cc638328713fe427aaeccb9b253a609f47310dda
thaide2k <thaide2k@users.noreply.github.com>
2026-04-17 03:04:44 +0000

    chore: vercel preview support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/ignisboost/',
+export default defineConfig(() => {
+  const isVercel = process.env.VERCEL === '1'
+
+  return {
+    plugins: [react()],
+    base: isVercel ? '/' : '/ignisboost/',
+  }
 })
